30a847e3132ffa460e881c90c5a5097404667fdc
EnzoCam7 <camerac09@gmail.com>
2022-06-29 19:48:13 -0300

    First part with the components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+import { TodoItem } from './components/TodoItem';
+import { CreateTodoButton } from './components/CreateTodoButton';
+import { TodoList } from './components/TodoList';
+import { TodoCounter } from './components/TodoCounter';
+import { TodoSearch } from './components/TodoSearch';
+
+const todos = [
+  {text: 'Cortar cebolla', completed: false},
+  {text: 'Terminar la app de TODOs', completed: false},
+  {text: 'Curarme del hombro', completed: false}
+]
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <React.Fragment> {/*Evitamos estar enviando div's todo el rato.*/}
+      <TodoCounter />
+  
+      <TodoSearch />
+      <input placeholder='Cebollita' />
+  
+      <TodoList>
+        {todos.map(todo => (
+          <TodoItem 
+            key={todo.text}
+            text={todo.text}
+          />
+        ))}
+      </TodoList>
+  
+      <CreateTodoButton />
+    </React.Fragment> //
+  )
 }
 
 export default App;
---
+++ src/components/TodoCounter.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function TodoCounter() {
+    return(
+        <h2>Completaste X de Y Todo's</h2>
+    )
+}
+
+export {TodoCounter}
